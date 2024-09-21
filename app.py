9a7d3251695504b48c4cbd775f13f2e21efe98b1
Bruhmin001 <khush.trivedi005@nmims.edu.in>
2024-09-21 16:14:18 +0530

    added_data_Selector

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 import streamlit as st
-from functions import * 
+import pandas as pd
+
 st.title('ML on Web')
 
 model_sidebar = st.sidebar.radio("Choose a ML Model", ('Linear Regression', 'Logistic Regression'))
-start_button = st.button("Start")
-if start_button:
-    start()
+file_upload = st.file_uploader("Upload Dataset",type='csv')
+try:
+    df = pd.read_csv(file_upload)
+    df.to_csv("from_user.csv")
+    col1 , col2 = st.columns(2)
+    with col1:
+        vars = st.multiselect("Choose the Variables",df.columns)
+        X = df[vars]
+        st.write(X)
+    with col2:
+        target = st.selectbox("Choose the Target Variable",df.columns)
+        Y = df[target]
+        st.write(Y)
+    adv_setting = st.toggle("Advanced Settings")
+    if adv_setting:
+        test_train_ratio = st.slider("Test Train Split Ratio",0.0,1.0,0.8)
+        random_state = st.text_input("Enter Random State")
+    if Y.name in X.columns:
+        st.write("Target variable should not be in the list of variables")
+    else:
+        st.button("Cook it")
+
+except ValueError:
+    st.write("Please upload a file") 
+
--- functions.py
+++ functions.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
+from app import *
 
-def start():
-    st.title('ML on Web')
+df = pd.read_csv("from_user.csv")
+print(df)
+
+def get_column(df):
+    return df.columns
+
+X,Y = send_data()

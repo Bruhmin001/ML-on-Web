5bec9d25f944ddc128e33bc7ea14923693e44748
Bruhmin001 <khush.trivedi005@nmims.edu.in>
2024-09-24 15:24:56 +0530

    added models

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import pandas as pd
+from functions import *
+
 
 st.title('ML on Web')
 
-model_sidebar = st.sidebar.radio("Choose a ML Model", ('Linear Regression', 'Logistic Regression'))
+model_sidebar = st.sidebar.radio("Choose a ML Model", ('Linear Regression', 'Logistic Regression','EDA Graphs'))
 file_upload = st.file_uploader("Upload Dataset",type='csv')
+
+
 try:
     df = pd.read_csv(file_upload)
-    df.to_csv("from_user.csv")
     col1 , col2 = st.columns(2)
     with col1:
         vars = st.multiselect("Choose the Variables",df.columns)
@@ -19,13 +22,22 @@ try:
         st.write(Y)
     adv_setting = st.toggle("Advanced Settings")
     if adv_setting:
-        test_train_ratio = st.slider("Test Train Split Ratio",0.0,1.0,0.8)
-        random_state = st.text_input("Enter Random State")
+        test_train_ratio = st.slider("Test Train Split Ratio",0.0,1.0,0.2)
+        random_states = st.text_input("Enter Random State")
+
     if Y.name in X.columns:
         st.write("Target variable should not be in the list of variables")
     else:
-        st.button("Cook it")
-
+        submitted = st.button("Cook it")
+    if submitted:
+        if model_sidebar == 'Linear Regression':
+            Linearregression(X,Y)
+        if model_sidebar =='Logistic Regression':
+            Logisticregression(X,Y)
+        
 except ValueError:
-    st.write("Please upload a file") 
+    st.write("Please upload a dataset")
+
+
+
 
--- functions.py
+++ functions.py
@@ -1,11 +1,20 @@
+from sklearn.linear_model import LinearRegression,LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error,accuracy_score,confusion_matrix
 import streamlit as st
-import pandas as pd
-from app import *
 
-df = pd.read_csv("from_user.csv")
-print(df)
+def Linearregression(X,Y,train_test_ratio=0.2,random_states=1):
+            X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=train_test_ratio, random_state=int(random_states))
+            model = LinearRegression()
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            st.write("Mean Squared Error: ",mean_squared_error(y_test,y_pred))
+            st.write("Model Score: ",model.score(X_test,y_test))
+            st.write("Accuracy Score: ",accuracy_score(y_test, y_pred))
 
-def get_column(df):
-    return df.columns
-
-X,Y = send_data()
+def Logisticregression(X,Y,train_test_ratio=0.2,random_states=1):
+            X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=train_test_ratio, random_state=int(random_states))
+            model = LogisticRegression()
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            st.write("Accuracy Score: ",confusion_matrix(y_test, y_pred))
